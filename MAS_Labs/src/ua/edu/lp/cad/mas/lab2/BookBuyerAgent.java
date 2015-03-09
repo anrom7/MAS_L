d2d67c36c532295a9d235701f808d0b5556cd979
Andrii Hurskyi <tyran53red@gmail.com>
2015-03-10 00:51:44 +0200

    Added lab1 and lab2

---
+++ MAS_Labs/src/ua/edu/lp/cad/mas/lab2/BookBuyerAgent.java
@@ -0,0 +1,191 @@
+package ua.edu.lp.cad.mas.lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class BookBuyerAgent extends Agent {
+	private static final long serialVersionUID = -6300630449468161409L;
+
+	// The title of the book to buy
+	private List<String> targetBookTitles = new ArrayList<String>();
+	// The list of known seller agents
+	private AID[] sellerAgents = null;
+	
+	private BookBuyerGui gui = null;
+	
+	private List<BookHandler> bookHandlers = new ArrayList<BookBuyerAgent.BookHandler>();
+
+	// Put agent initializations here
+	protected void setup() {
+		
+		gui = new BookBuyerGui(this);
+		gui.showGui();
+		
+		System.out.println("Hello! Buyer-agent " + getAID().getName() + " is ready.");
+		
+		addBehaviour(new TickerBehaviour(this, 15000) {
+			private static final long serialVersionUID = -2539521521322352923L;
+
+			protected void onTick() {
+				// Update the list of seller agents
+				DFAgentDescription template = new DFAgentDescription();
+				ServiceDescription sd = new ServiceDescription();
+				sd.setType("book-selling");
+				template.addServices(sd);
+				
+				try {
+					DFAgentDescription[] result = DFService.search(myAgent, template);
+					sellerAgents = new AID[result.length];
+					
+					for (int i = 0; i < result.length; ++i) {
+						sellerAgents[i] = result[i].getName();
+					}
+				} catch (FIPAException fe) {
+					fe.printStackTrace();
+				}
+				
+				for (String title : targetBookTitles) {
+					myAgent.addBehaviour(new RequestPerformer(title));
+				}
+			}
+		});
+	}
+	
+	public void addBook(String title) {
+		System.out.println("Added book " + title);
+		
+		targetBookTitles.add(title);
+	}
+	
+	public void addBookHandler(BookHandler bookHandler) {
+		this.bookHandlers.add(bookHandler);
+	}
+	
+	public void removeBookHandler(BookHandler bookHandler) {
+		this.bookHandlers.remove(bookHandler);
+	}
+
+	private class RequestPerformer extends Behaviour {
+		private static final long serialVersionUID = 1820263965187147270L;
+		
+		private AID bestSeller; // The agent who provides the best offer
+		private int bestPrice; // The best offered price
+		private int repliesCnt = 0; // The counter of replies from seller agents
+		private MessageTemplate mt; // The template to receive replies
+		private int step = 0;
+
+		public String targetBookTitle = null;
+		
+		public RequestPerformer(String targetBookTitle) {
+			this.targetBookTitle = targetBookTitle;
+		}
+		
+		public void action() {
+			switch (step) {
+			case 0:
+				// Send the cfp to all sellers
+				ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+				for (int i = 0; i < sellerAgents.length; ++i) {
+					cfp.addReceiver(sellerAgents[i]);
+				}
+				
+				cfp.setContent(targetBookTitle);
+				cfp.setConversationId("book-trade");
+				cfp.setReplyWith("cfp" + System.currentTimeMillis()); // Unique value
+				myAgent.send(cfp);
+				// Prepare the template to get proposals
+				mt = MessageTemplate.and(
+						MessageTemplate.MatchConversationId("book-trade"),
+						MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+				step = 1;
+				break;
+			case 1:
+				// Receive all proposals/refusals from seller agents
+				ACLMessage reply = myAgent.receive(mt);
+				if (reply != null) {
+					// Reply received
+					if (reply.getPerformative() == ACLMessage.PROPOSE) {
+						// This is an offer
+						int price = Integer.parseInt(reply.getContent());
+						if (bestSeller == null || price < bestPrice) {
+							// This is the best offer at present
+							bestPrice = price;
+							bestSeller = reply.getSender();
+						}
+					}
+					
+					repliesCnt++;
+					
+					if (repliesCnt >= sellerAgents.length) {
+						// We received all replies
+						step = 2;
+					}
+				} else {
+					block();
+				}
+				break;
+			case 2:
+				// Send the purchase order to the seller that provided the best
+				// offer
+				ACLMessage order = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+				order.addReceiver(bestSeller);
+				order.setContent(targetBookTitle);
+				order.setConversationId("book-trade");
+				order.setReplyWith("order" + System.currentTimeMillis());
+				myAgent.send(order);
+				// Prepare the template to get the purchase order reply
+				mt = MessageTemplate.and(
+						MessageTemplate.MatchConversationId("book-trade"),
+						MessageTemplate.MatchInReplyTo(order.getReplyWith()));
+				step = 3;
+				break;
+			case 3:
+				// Receive the purchase order reply
+				reply = myAgent.receive(mt);
+				if (reply != null) {
+					// Purchase order reply received
+					if (reply.getPerformative() == ACLMessage.INFORM) {
+						// Purchase successful. We can terminate
+						System.out.println(targetBookTitle + " successfully purchased.");
+						
+						for (BookHandler handler : bookHandlers) {
+							handler.onBookBay(targetBookTitle);
+						}
+						
+						System.out.println("Price = " + bestPrice);
+					}
+					step = 4;
+				} else {
+					block();
+				}
+				break;
+			}
+		}
+
+		public boolean done() {
+			return ((step == 2 && bestSeller == null) || step == 4);
+		}
+	}
+	
+	@Override
+	protected void takeDown() {
+		gui.dispose();
+		
+		super.takeDown();
+	}
+
+	public interface BookHandler {
+		public void onBookBay(String title);
+	}
+}
---
+++ MAS_Labs/src/ua/edu/lp/cad/mas/lab2/BookBuyerGui.java
@@ -0,0 +1,139 @@
+package ua.edu.lp.cad.mas.lab2;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.AbstractTableModel;
+
+import ua.edu.lp.cad.mas.lab2.BookBuyerAgent.BookHandler;
+
+public class BookBuyerGui extends JFrame {
+	private static final long serialVersionUID = 1331346802753304828L;
+
+	private JTextField titleField = new JTextField();
+	
+	private BooksModel booksModel = null;
+	private JTable tblBooks = null;
+	
+	public BookBuyerGui(BookBuyerAgent a) {
+		super(a.getLocalName());
+
+		booksModel = new BooksModel(a);
+		tblBooks = new JTable(booksModel);
+		
+		JPanel pnlMain = new JPanel(new BorderLayout());
+		
+		pnlMain.add(new JLabel("Books list"), BorderLayout.NORTH);
+		pnlMain.add(tblBooks, BorderLayout.CENTER);
+		
+		JPanel pnlBottom = new JPanel(new BorderLayout());
+		pnlBottom.add(new JLabel("Book title:"), BorderLayout.WEST);
+		pnlBottom.add(titleField, BorderLayout.CENTER);
+		
+		JButton addButton = new JButton("Add");
+		addButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				try {
+					String title = titleField.getText().trim();
+					booksModel.addBook(title);
+					titleField.setText("");
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(BookBuyerGui.this,
+							"Invalid values. " + e.getMessage(), "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		
+		pnlBottom.add(addButton, BorderLayout.EAST);
+		
+		pnlMain.add(pnlBottom, BorderLayout.SOUTH);
+
+		getContentPane().add(pnlMain, BorderLayout.CENTER);
+
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				booksModel.shootDown();
+			}
+		});
+	}
+
+	public void showGui() {
+		pack();
+		setMinimumSize(new Dimension(500, 250));
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int centerX = (int) screenSize.getWidth() / 2;
+		int centerY = (int) screenSize.getHeight() / 2;
+		setLocation(centerX - getWidth() / 2, centerY - getHeight() / 2);
+		super.setVisible(true);
+	}
+	
+	private class BooksModel extends AbstractTableModel {
+		private static final long serialVersionUID = -1563735225238755621L;
+		
+		private BookBuyerAgent myAgent = null;
+		private List<String> books = new ArrayList<String>();
+		
+		public BooksModel(BookBuyerAgent myAgent) {
+			this.myAgent = myAgent;
+			
+			myAgent.addBookHandler(new BookHandler() {
+				@Override
+				public void onBookBay(String title) {
+					books.remove(title);
+					fireTableDataChanged();
+				}
+			});
+		}
+		
+		@Override
+		public int getRowCount() {
+			return books.size();
+		}
+
+		@Override
+		public int getColumnCount() {
+			return 1;
+		}
+
+		@Override
+		public String getColumnName(int column) {
+			return "Title";
+		}
+
+		@Override
+		public Class<?> getColumnClass(int columnIndex) {
+			return String.class;
+		}
+
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			return books.get(rowIndex);
+		}
+		
+		public void addBook(String title) {
+			myAgent.addBook(title);
+			books.add(title);
+			fireTableDataChanged();
+		}
+		
+		public void shootDown() {
+			myAgent.doDelete();
+		}
+	}
+}
