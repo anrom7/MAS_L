79116ccaf270ec854d9666b37a41c14841b21e02
josik <josik4ke@gmail.com>
2015-03-12 21:09:18 +0200

    Lab #1 done:  - PingPong project implemented;  - Changed project name and agents name in arguments.

---
+++ Lab1/PingPongHadzeraLabOne/src/ua/agentlab/PongAgent.java
@@ -0,0 +1,27 @@
+package ua.agentlab;
+
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+
+public class PongAgent extends Agent {
+private static final long serialVersionUID = 3663966406239393054L;
+protected void setup() {
+addBehaviour(new CyclicBehaviour(this) {
+private static final long serialVersionUID = -1912882200351395625L;
+public void action() {
+ACLMessage msg = receive();
+if (msg != null) {
+                  // ¬ив≥д на екран локального ≥мен≥ агента ≥ отриманого пов≥домленн€
+                    System.out.println(" Ц " + myAgent.getLocalName() + " received: " + msg.getContent());
+                    ACLMessage reply = msg.createReply();
+                    reply.setPerformative(ACLMessage.INFORM); // встановлюЇмо перформатив пов≥домленн€
+                    reply.setContent("Pong"); // вм≥ст пов≥домленн€
+                    send(reply);// в≥дправка пов≥домленн€
+                }
+block();
+}
+});
+}
+}
+
