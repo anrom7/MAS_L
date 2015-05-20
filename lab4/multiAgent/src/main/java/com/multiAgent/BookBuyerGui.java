4d2ec285b8a7a7f49ed7ecae4d2b01a95333aed2
saper024 <saper024@gmail.com>
2015-05-20 22:12:49 +0300

    add classes from lab2: BookSellerAgent, BookSellerGui, BookBuyerGui

---
+++ lab4/multiAgent/src/main/java/com/multiAgent/BookBuyerGui.java
@@ -0,0 +1,69 @@
+package com.multiAgent;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+class BookBuyerGui extends JFrame {	
+
+	private static final long serialVersionUID = 1L;
+
+	private BookBuyerAgent myAgent;
+	
+	private JTextField titleField, priceField;
+	
+	BookBuyerGui(BookBuyerAgent a) {
+		super(a.getLocalName());
+		
+		myAgent = a;
+		
+		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(2, 2));
+		p.add(new JLabel("Book title to buy:"));
+		titleField = new JTextField(15);
+		p.add(titleField);
+		p.add(new JLabel("Price limit:"));
+		priceField = new JTextField(15);
+		p.add(priceField);
+		getContentPane().add(p, BorderLayout.CENTER);
+		
+		JButton addButton = new JButton("Add");
+		addButton.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				try {
+					String title = titleField.getText().trim();
+					String price = priceField.getText().trim();
+					myAgent.updateRecord(title, Integer.parseInt(price));
+					//myAgent.doDelete();
+					dispose();
+				}
+				catch (Exception e) {
+					JOptionPane.showMessageDialog(BookBuyerGui.this, "Invalid values. "+e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE); 
+				}
+			}
+		} );
+		p = new JPanel();
+		p.add(addButton);
+		getContentPane().add(p, BorderLayout.SOUTH);
+		
+		// Make the agent terminate when the user closes 
+		// the GUI using the button on the upper right corner	
+		addWindowListener(new	WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				myAgent.doDelete();
+			}
+		} );
+		
+		setResizable(false);
+	}
+	
+	public void showGui() {
+		pack();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int centerX = (int)screenSize.getWidth() / 2;
+		int centerY = (int)screenSize.getHeight() / 2;
+		setLocation(centerX - getWidth() / 2, centerY - getHeight() / 2);
+		super.setVisible(true);
+	}	
+}
